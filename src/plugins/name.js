aa2c1c7a490a2489a9b53c647ee7da1a4c5c1595
Les Orchard <me@lmorchard.com>
2015-02-11 23:50:02 -0500

    Add findEntityByName utility to name component

--- src/plugins/name.js
+++ src/plugins/name.js
@@ -4,6 +4,15 @@ export class Name extends Core.Component {
   static defaults() {
     return { name: "unnamed" };
   }
+  static findEntityByName(world, name) {
+    var names = world.entities.get('Name');
+    for (var nid in names) {
+      var nameComponent = names[nid];
+      if (nameComponent.name == name) {
+        return nid;
+      }
+    }
+  }
 }
 
 Core.registerComponent('Name', Name);
