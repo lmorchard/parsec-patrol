4a6e4d23a8cb6f9da69fbdd7b3e24d06f8409c97
Les Orchard <me@lmorchard.com>
2015-02-11 23:50:29 -0500

    Port over Seeker component and system

--- src/plugins/seeker.js
+++ src/plugins/seeker.js
@@ -2,3 +2,95 @@ import * as Core from "../core";
 
 import "./position";
 import "./motion";
+
+import Vector2D from "../lib/Vector2D";
+
+export class Seeker extends Core.Component {
+  static defaults() {
+    return {
+      targetName: null,
+      targetEntityId: null,
+      targetPosition: null,
+      acquisitionDelay: 0,
+      radPerSec: Math.PI
+    };
+  }
+}
+
+Core.registerComponent('Seeker', Seeker);
+
+export class SeekerSystem extends Core.System {
+
+  matchComponent() { return 'Seeker'; }
+
+  initialize() {
+    this.vSeeker = new Vector2D();
+    this.vTarget = new Vector2D();
+  }
+
+  updateComponent(timeDelta, entityId, seeker) {
+
+    // Look up the orbited entity ID, if only name given.
+    if (seeker.targetName && !seeker.targetEntityId) {
+      seeker.targetEntityId = Core.getComponent('Name')
+        .findEntityByName(this.world, seeker.targetName);
+    }
+
+    // Process a delay before the seeker "acquires" the target and
+    // starts steering. Makes missiles look interesting.
+    if (seeker.acquisitionDelay > 0) {
+      seeker.acquisitionDelay -= timeDelta;
+      return;
+    }
+
+    var position = this.world.entities.get('Position', entityId);
+    var motion = this.world.entities.get('Motion', entityId);
+    if (!position || !motion) { return; }
+
+    // Accept either a raw x/y coord or entity ID as target
+    var targetPosition = seeker.targetPosition;
+    if (!targetPosition) {
+      targetPosition = this.world.entities.get('Position', seeker.targetEntityId);
+    }
+    if (!targetPosition || !(targetPosition.x && targetPosition.y)) { return; }
+
+    // Set up the vectors for angle math...
+    this.vSeeker.setValues(position.x, position.y);
+    this.vTarget.setValues(targetPosition.x, targetPosition.y);
+
+    // Get the target angle, ensuring a 0..2*Math.PI range.
+    var targetAngle = this.vSeeker.angleTo(this.vTarget)
+    if (targetAngle < 0) {
+      targetAngle += (2 * Math.PI);
+    }
+
+    // Pick the direction from current to target angle
+    var direction = (targetAngle < position.rotation) ? -1 : 1;
+
+    // If the offset between the angles is more than half a circle, go
+    // the other way because it'll be shorter.
+    var offset = Math.abs(targetAngle - position.rotation);
+    if (offset > Math.PI) {
+      direction = 0 - direction;
+    }
+
+    // Work out the desired delta-rotation to steer toward target
+    var targetDr = direction * Math.min(seeker.radPerSec, offset / timeDelta);
+
+    // Calculate the delta-rotation impulse required to meet the goal,
+    // but constrain to the capability of the steering thrusters
+    var impulseDr = (targetDr - motion.drotation);
+    if (Math.abs(impulseDr) > seeker.radPerSec) {
+      if (impulseDr > 0) {
+        impulseDr = seeker.radPerSec;
+      } else if (impulseDr < 0) {
+        impulseDr = 0 - seeker.radPerSec;
+      }
+    }
+    motion.drotation += impulseDr;
+
+  }
+
+}
+
+Core.registerSystem('Seeker', SeekerSystem);
---
+++ src/sketches/seeker/index.js
@@ -0,0 +1,47 @@
+import * as Core from "../../core";
+
+import "../../plugins/name";
+import "../../plugins/position";
+import "../../plugins/motion";
+import "../../plugins/thruster";
+import "../../plugins/orbiter";
+import "../../plugins/seeker";
+import "../../plugins/canvasViewport";
+import "../../plugins/drawStats";
+
+var move = 0.07;
+var rot = (Math.PI / 2);
+
+var world = new Core.World({
+  systems: {
+    CanvasViewport: { container: '#game', canvas: '#viewport' },
+    DrawStats: {},
+    Motion: {},
+    Orbiter: {},
+    Thruster: {},
+    Seeker: {}
+  }
+});
+
+world.entities.insert({
+  Name: { name: 'sun'},
+  Position: {}
+}, {
+  Name: { name: 'alpha'},
+  Position: { x: 250, y: 250 },
+  Orbiter: { name: 'sun' }
+}, {
+  Name: { name: 'chaser1'},
+  Position: {},
+  Motion: {},
+  Thruster: { deltaV: 400, maxV: 175 },
+  Seeker: { targetName: 'alpha', radPerSec: 0.9 }
+}, {
+  Name: { name: 'chaser2'},
+  Position: {},
+  Motion: {},
+  Thruster: { deltaV: 600, maxV: 400 },
+  Seeker: { targetName: 'alpha', radPerSec: 2 }
+});
+
+world.start();
